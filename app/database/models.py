6cd924523b85adea4c2d478aed6463ae48fa8b02
qodirod <qodirod@qodirod>
2026-05-16 21:57:47 -0400

    Implementing database

---
+++ app/database/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import BigInteger, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
+
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+
+class Teams(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25))
+
+class Game(Base):
+    __tablename__ = 'items'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25))
+    description: Mapped[str] = mapped_column(String(120))
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from aiogram import Bot, Dispatcher, F
 
 
 from app.handlers import router
+from app.database.models import async_main
 
 
 async def main():
+    await async_main()
     bot = Bot(token='8887834947:AAHdjq0CU9soSG0xaVs2FAmg3nqpMe3yNyY')
     dp = Dispatcher()
     dp.include_router(router)
